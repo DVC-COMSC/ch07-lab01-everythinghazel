5b89bbc43aea3a9921303fd3508aa88814b2f7eb
everythinghazel <112515847+everythinghazel@users.noreply.github.com>
2022-11-22 21:52:55 +0000

    1

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-
 # ******************************
-# Make your Code
+nums = [ ]
+
+for i in range(5):
+    inputval = list(map(int, input("Enter input: ").split()))
+    # nums.append(input)
+print(inputval)
+
+inputval.pop(max(inputval))
+inputval.pop(min(inputval))
+
+# sumval = 0
+# inputval.remove(max(input))
+# inputval.remove(min(input))
+
+# sumval += nums[i]
+print(inputval)
+
 # ******************************
